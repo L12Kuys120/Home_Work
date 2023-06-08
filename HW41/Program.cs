e620dd8edf022d1720cb52f916fc3b987ed3b150
Mikhail <radionetcom@inbox.ru>
2023-06-08 15:24:21 +0300

    Add folders with home work Sem6

---
+++ HW41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите элементы массива через пробел: ");
+string elements = Console.ReadLine();
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ");   
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int PlusArray(int[] call)
+{
+    int result = 0;
+    foreach (var item in call)
+    {
+        result += item > 0 ? 1 : 0;
+    }
+    return result;
+}
+
+int[] baseArray = GetArrayFromString(elements);
+Console.WriteLine(String.Join(" ",baseArray));
+int sumPlus = PlusArray(baseArray);
+Console.WriteLine($"Введено {sumPlus} положительных чисел");
---
+++ HW45/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива 
+//с помощью поэлементного копирования.
+
+void FillArray(int[] call)
+{    
+    for (int i = 0; i < call.Length; i++)
+    {
+        call[i] = new Random().Next(232);
+    }    
+}
+
+int[] CopyArray(int[] call)
+{
+    int[] call2 = new int[call.Length];
+    for (int i = 0; i < call.Length; i++)
+    {
+        call2[i] = call[i];
+    }  
+    return call2;
+}
+
+int[] array = new int[234];
+FillArray(array);
+Console.WriteLine($"real array -  {String.Join(" ",array)}");
+int[] array2 = CopyArray(array);
+Console.WriteLine($"copy array -  {String.Join(" ",array2)}");
+
